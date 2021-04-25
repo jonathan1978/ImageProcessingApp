826940ac49e2ee18a7f5d0225edb94984e5b868d
Jonathan Lawerh <jonathan@cloudport.tech>
2021-04-25 12:24:46 +0000

    Added A log to check if Image Processed

--- src/utilities/images.ts
+++ src/utilities/images.ts
@@ -15,8 +15,7 @@ async function transform(imageName: string) {
           await sharp(`./images/${imageName}`)
             .resize(300, 200)
             .toFile(outputFile, function(err) {
-              // output.jpg is a 300 pixels wide and 200 pixels high image
-              // containing a scaled and cropped version of input.jpg
+              console.log("Image Processed");
           });
           return imageName;
         }
