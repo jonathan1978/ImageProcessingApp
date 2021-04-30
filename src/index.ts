cd75737d996a36cf1c6cbb1247bd06e09acfe125
Jonathan Lawerh <jonathan@cloudport.tech>
2021-04-30 10:30:51 +0000

    Updated Eslint and Prettier Configuration

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,28 @@
-import express from 'express'
-import images from './utilities/images'
-import path from 'path'
+import express from 'express';
+import images from './utilities/images';
+import path from 'path';
 import isValid from 'is-valid-path';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/api/images', async (req, res) => {
-  const fileName = req.query.fileName as string
+app.get('/api/images', async (req: express.Request, res: express.Response) => {
+  const fileName = req.query.fileName as string;
   if (fileName !== undefined) {
     images.transform(fileName).then(response => {
-        if (isValid(response)) {
-            res.sendFile(path.resolve(response));
-        }          
-        else {
-            res.send(response);
-        }
-      
-    })
+      if (isValid(response)) {
+        res.sendFile(path.resolve(response));
+      } else {
+        res.send(response);
+      }
+    });
   } else {
-    res.send('File Name is undefined')
+    res.send('File Name is undefined');
   }
-})
+});
 
 //start the Express server
 app.listen(port, () => {
-  console.log(`server started at http://localhost:${port}`)
-})
-export default app
+  console.log(`server started at http://localhost:${port}`);
+});
+export default app;
--- src/utilities/images.ts
+++ src/utilities/images.ts
@@ -1,25 +1,24 @@
-import fs from "fs";
-import {promises as fsPromises} from "fs";
-import { constants } from 'fs';
-import sharp from "sharp";
+import fs from 'fs';
+import sharp from 'sharp';
 
 async function transform(imageName: string) {
-    try {
-        let outputFile = `./converted/${imageName}`;
-        if (fs.existsSync(outputFile)) {
-          return outputFile;
-        } else {
-          if (fs.existsSync(`./images/${imageName}`)) {
-            await sharp(`./images/${imageName}`).resize(300, 200).toFile(outputFile);
-            return outputFile;
-          }
-          else {
-            return "**Image does not exist**";
-          }
-        }
-      } catch (error) {
-        return error;
+  try {
+    const outputFile = `./converted/${imageName}`;
+    if (fs.existsSync(outputFile)) {
+      return outputFile;
+    } else {
+      if (fs.existsSync(`./images/${imageName}`)) {
+        await sharp(`./images/${imageName}`)
+          .resize(300, 200)
+          .toFile(outputFile);
+        return outputFile;
+      } else {
+        return '**Image does not exist**';
       }
+    }
+  } catch (error) {
+    return error;
+  }
 }
 
-export default {transform}
+export default { transform };
