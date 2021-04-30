cd2b2e4ff4012e158f5a4e8e73834a6da1e906c7
Jonathan Lawerh <jonathan@cloudport.tech>
2021-04-30 13:39:54 +0000

    Fixed Missing Return Type on Transform Function

--- src/utilities/images.ts
+++ src/utilities/images.ts
@@ -1,23 +1,25 @@
 import fs from 'fs';
 import sharp from 'sharp';
 
-async function transform(imageName: string) {
+async function transform(imageName: string): Promise<string> {
   try {
     const outputFile = `./converted/${imageName}`;
+    let formattedFile = "";
     if (fs.existsSync(outputFile)) {
-      return outputFile;
+      formattedFile = outputFile;
     } else {
       if (fs.existsSync(`./images/${imageName}`)) {
         await sharp(`./images/${imageName}`)
           .resize(300, 200)
           .toFile(outputFile);
-        return outputFile;
+          formattedFile = outputFile;
       } else {
         return '**Image does not exist**';
       }
     }
+    return formattedFile;
   } catch (error) {
-    return error;
+    return `Error: ${error}`;
   }
 }
 
